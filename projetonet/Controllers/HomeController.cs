17265ae46cccd0069ab15417e083956e2c4aed61
Julio César Barbosa da Silva <j.barbosa.da.silva@avanade.com>
2024-07-05 23:28:44 -0300

    Fix

--- projetonet/Controllers/HomeController.cs
+++ projetonet/Controllers/HomeController.cs
@@ -1,13 +1,31 @@
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using projetonet.Models;
 
-namespace MeuProjeto.Controllers
+namespace projetonet.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        public IActionResult Index()
-        {
-            ViewBag.Message = "Hello World!";
-            return View();
-        }
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
